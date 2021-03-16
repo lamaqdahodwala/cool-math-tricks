d817dd5f2ba16091f6fed71fdef1d8b77f682436
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-03-16 17:05:34 -0400

    hooray static is working

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, url_for
+
+app = Flask('app')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+app.run(host='127.0.0.1', port=5500)
