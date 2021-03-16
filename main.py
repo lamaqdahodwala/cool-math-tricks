0bd558741ff0780a36c384cb86a97dd5aad3ad25
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-03-16 17:10:25 -0400

    new file heirarchy

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ app = Flask('app')
 def home():
     return render_template('index.html')
 
+@app.route('/answeris5')
+def answeris5():
+    return render_template('answeris5/index.html')
 app.run(host='127.0.0.1', port=5500)
